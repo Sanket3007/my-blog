85ee95253cbac39828f00e432d2de1a95fe71f3a
Sanket3007 <karanjesanket1980@gmail.com>
2021-12-06 14:46:53 +0530

    Using flexbox in React.js

--- src/page/home/Home.jsx
+++ src/page/home/Home.jsx
@@ -1,11 +1,17 @@
 import Header from '../../components/header/Header';
+import Post from '../../components/post/Post';
+import Sidebar from '../../components/sidebar/Sidebar';
 import './home.css';
+
 
 export default function Home() {
     return (
-        <div className="home">
+        <>
             <Header />
-            homepage
-        </div>
+            <div className="home">
+                <Post />
+                <Sidebar />
+            </div>
+        </>
     )
 }
