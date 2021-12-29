72f7826d44b6fd38519d2cd6d461cb967d95e406
Sanket3007 <karanjesanket1980@gmail.com>
2021-12-29 20:09:32 +0530

    Login-Register page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import Topbar from "./components/topbar/Topbar";
 import Home from "./page/home/Home";
+import Login from "./page/login/Login";
+import Register from "./page/register/Register";
 import Settings from "./page/settings/Settings";
 import Single from "./page/single/Single";
 import Write from "./page/write/Write";
@@ -8,7 +10,7 @@ function App() {
   return (
     <>
       <Topbar />
-      <Settings />
+      <Register />
     </>
   );
 }
