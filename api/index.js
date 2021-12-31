3dd6b52de4dfa2ff68259a69a9407f04b51c5a0a
Sanket3007 <karanjesanket1980@gmail.com>
2021-12-31 13:41:14 +0530

    Nodejs and Mongodb connnection

--- api/index.js
+++ api/index.js
@@ -1,8 +1,19 @@
 const express = require("express");
 const app = express();
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+
+dotenv.config();
+
+// NodeJs MondoDb Connection
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(console.log("Connected to MongoDB"))
+  .catch((err) => console.log(err));
 
 console.log("sanket");
 
+// Setting up the expressJs and listening to the port
 app.listen("5000", () => {
     console.log("Backend is running.");
   });
