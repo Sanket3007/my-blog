e2a8d7af539071f0dd88beb4df903e2b9e1f1b15
Sanket3007 <karanjesanket1980@gmail.com>
2021-12-31 15:08:03 +0530

    POST CRUD

--- api/index.js
+++ api/index.js
@@ -4,6 +4,9 @@ const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 
 const authRoute = require("./routes/auth");
+const userRoute = require("./routes/users");
+const postRoute = require("./routes/posts");
+const categoryRoute = require("./routes/categories");
 
 dotenv.config();
 app.use(express.json()); // Use to send any JSON object 
@@ -14,9 +17,12 @@ mongoose
   .then(console.log("Connected to MongoDB"))
   .catch((err) => console.log(err));
 
-console.log("sanket");
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/posts", postRoute);
+app.use("/api/categories", categoryRoute);
 
 // Setting up the expressJs and listening to the port
 app.listen("5000", () => {
-    console.log("Backend is running.");
-  });
+  console.log("Backend is running.");
+});
